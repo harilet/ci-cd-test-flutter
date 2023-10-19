780a8c5121b487ac2de6652836b1cf0646aac986
harilet <40688601+harilet@users.noreply.github.com>
2023-10-19 22:53:23 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Hi welcome'
+              'PR test'
             ),
             const Text(
               'You have pushed the button this many times:',
